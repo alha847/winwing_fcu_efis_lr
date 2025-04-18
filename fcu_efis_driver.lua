be07512b79b8885cae9eea9d5d9ee88357a8d0a2
alha847 <201389857+alha847@users.noreply.github.com>
2025-04-18 22:26:10 +0200

    Added LCD support for EFIS R

--- fcu_efis_driver.lua
+++ fcu_efis_driver.lua
@@ -276,9 +276,10 @@ dataref("wpt_r", "sim/cockpit2/EFIS/EFIS_fix_on_copilot", "readonly")
 dataref("vord_r", "sim/cockpit2/EFIS/EFIS_vor_on_copilot", "readonly")
 dataref("ndb_r", "sim/cockpit2/EFIS/EFIS_ndb_on_copilot", "readonly")
 dataref("arpt_r", "sim/cockpit2/EFIS/EFIS_airport_on_copilot", "readonly")
-dataref("baro_rrr", "sim/cockpit2/gauges/actuators/barometer_setting_in_hg_copilot", "readonly")
-dataref("baro_inhg_r", "laminar/A333/barometer/fo_inHg_hPa_pos", "readonly")
+dataref("baro_value_r", "sim/cockpit2/gauges/actuators/barometer_setting_in_hg_copilot", "readonly") --baro value in inHg
+dataref("baro_inhg_r", "laminar/A333/barometer/fo_inHg_hPa_pos", "readonly") --0=inhg, 1=hpa
 dataref("map_mode_r", "laminar/A333/knobs/EFIS_mode_pos_fo", "readonly")
+dataref("baro_mode_r", "laminar/A333/barometer/fo_mode", "readonly") --0=qnh, 1=std
 
 cache_data={}
 --fcu
@@ -306,9 +307,10 @@ cache_data["wpt_r"] = 0
 cache_data["vord_r"] = 0
 cache_data["ndb_r"] = 0
 cache_data["arpt_r"] = 0
-cache_data["baro_rrr"] = 0
+cache_data["baro_value_r"] = 0
 cache_data["baro_inhg_r"] = 0
 cache_data["map_mode_r"] = 0
+cache_data["baro_mode_r"] = 0
 --todo probably incomplete, e.g. baro unit missing
 
 --define led 
@@ -342,6 +344,7 @@ led_list_efis_r = {
 
 --define lcd
 local lcd_flags_fcu = {}
+--FCU
 lcd_flags_fcu["spd"] = {byte = 1, mask = 0x08, value = 1}
 lcd_flags_fcu["mach"] = {byte = 1, mask = 0x04, value = 0}
 lcd_flags_fcu["hdg"] = {byte = 0, mask = 0x80, value = 0}
@@ -364,13 +367,20 @@ lcd_flags_fcu["fvs"] = {byte = 10, mask = 0x40, value = 1}
 lcd_flags_fcu["ffpa2"] = {byte = 10, mask = 0x80, value = 0}
 lcd_flags_fcu["fpa_comma"] = {byte = 9, mask = 0x10, value = 0}
 lcd_flags_fcu["mach_comma"] = {byte = 12, mask = 0x01, value = 0}
+--EFIS R
+local lcd_flags_efisr = {}
+lcd_flags_efisr["qfe"] = {byte = 0, mask = 0x01, value = 1} --todo check value
+lcd_flags_efisr["qnh"] = {byte = 0, mask = 0x02, value = 1} --todo check value
+lcd_flags_efisr["unitInHg"] = {byte = 2, mask = 0x80, value = 1}
+--lcd_flags_efisr["baro"] = {byte = , mask = , value = }
+--lcd_flags_efisr["std"] = {byte = , mask = , value = }
 
 --define lcd flags for EFIS R
 --todo check meaning of "value"
-local lcd_flags_efisr = {}
-lcd_flags_efisr["efisr_qfe"] = {byte = 0, mask = 0x01, value = 1}
-lcd_flags_efisr["efisr_qnh"] = {byte = 0, mask = 0x02, value = 1}
-lcd_flags_efisr["efisr_hpa_dec"] = {byte = 2, mask = 0x80, value = 0} --decimal point if unit "in Hg"
+--local lcd_flags_efisr = {}
+--lcd_flags_efisr["efisr_qfe"] = {byte = 0, mask = 0x01, value = 1}
+--lcd_flags_efisr["efisr_qnh"] = {byte = 0, mask = 0x02, value = 1}
+--lcd_flags_efisr["efisr_hpa_dec"] = {byte = 2, mask = 0x80, value = 0} --decimal point if unit "in Hg"
 
 function config_led(hiddev, led, dev)
     if (led.bind ~= "") then
@@ -483,65 +493,50 @@ end
 
 --todo added and converted AH
 --l = number of segments in the EFIS R LCD display -> 4
---input = value to be written, e.g. "29.92"
-function data_from_string_swapped_efis(l, input, unitInHg) -- next wired mapping :-)
+--input = value to be written as string, without points, e.g. "2992"
+function data_from_string_swapped_efis(l, input) 
 
-    --todo consider if input string is shorter than the 4 digits of the lcd 
-
-    logMsg("input = "..input)
-
-    --todo where does the +50 and /100 come from? maybe scaling?
-    --todo values <1000hpa currently not working
-
-    local input2 = 0
-    if (true) then --todo condition
-        input2 = round(input * 33.86389/100)--round((29.92 * 33.86388 + 50) / 100) 
+    --Ensure correct length of input string
+    if(len(input) ~= 4) then
+        return
     end
 
-    --logMsg("d = "..d2[0].."-"..d2[0].."-"..d2[0].."-"..d2[0])
-
+    --
     local n = {}
     for i=0,l do
         n[i] = 0
     end
-    --todo n = 0 * l
 
-    --d2[0] = 1
+    --Encode string letter by letter so that it can be sent via hid to the LCD.
+    --Target encoding is suitable for the FCU LCD.
+    local d = data_from_string(l, input) 
 
-    logMsg("input3 = "..input2)
-
-    --todo add explanation
-    d = data_from_string(l, input2) --input
-    --logMsg("d = "..d2[0].."-"..d2[0].."-"..d2[0].."-"..d2[0])
-    --logMsg("d = "..d2[0].."-"..d2[0].."-"..d2[0].."-"..d2[0])
-    --logMsg("d = "..d2[0].."-"..d2[0].."-"..d2[0].."-"..d2[0])
-    --logMsg("d = "..d2[0].."-"..d2[0].."-"..d2[0].."-"..d2[0])
-
-    logMsg("l = "..l)
-
-    -- fix wired segment mapping
-    --attention:lua does not consider 0 as false, so use something like x>0
-    --attention:lua "and" is no bitwise comparison, use bit.band
-    for i =0,l-1 do --todo check for condition
+    --Convert the encoding for the FCU LCD to the encoding for the  EFIS R LCD, which are 
+    --not the same (for an unknown reason). The two-step process with the FCU encoding in
+    --between has been copied from one of the repos where this is forked from. Probably
+    --it is caused by the order in which the code was implemented.
+    for i =0,l-1 do 
         n[i] = bit.bor(n[i], bit.band(d[i],0x08)>0 and 0x01 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x04)>0 and 0x02 or 0x0 )--|= 0x02 if d[i] & 0x04 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x04)>0 and 0x02 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x02)>0 and 0x04 or 0x0 )--|= 0x04 if d[i] & 0x02 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x02)>0 and 0x04 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x10)>0 and 0x08 or 0x0 )--|= 0x08 if d[i] & 0x10 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x10)>0 and 0x08 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x80)>0 and 0x10 or 0x0 )--|= 0x10 if d[i] & 0x80 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x80)>0 and 0x10 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x40)>0 and 0x20 or 0x0 )--|= 0x20 if d[i] & 0x40 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x40)>0 and 0x20 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x20)>0 and 0x40 or 0x0 )--|= 0x40 if d[i] & 0x20 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x20)>0 and 0x40 or 0x0 )
         logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
-        n[i] = bit.bor(n[i], bit.band(d[i],0x01)>0 and 0x80 or 0x0 )--|= 0x80 if d[i] & 0x01 else 0
+        n[i] = bit.bor(n[i], bit.band(d[i],0x01)>0 and 0x80 or 0x0 )
     end
 
-    logMsg("n = "..n[0].."-"..n[1].."-"..n[2].."-"..n[3])
+    --n contains the string to be written to the EFIS R LCD encoded in a format that 
+    --can be sent to the LCD via the hid protocol.
     return n
+
 end
 
 function fix_str_len(input, len)
@@ -574,19 +569,7 @@ function draw_efisr_lcd(fcu, baro_r)
 
     --logMsg("baro_r = "..baro_r)
 
-    local b = data_from_string_swapped_efis(4, baro_r, false)
-
-    --todo debug:show 7777 on efis r lcd
-    --b[0] = 0x70;
-    --b[1] = 0x70;
-    --b[2] = 0x70;
-    --b[3] = 0x70;
-
-    --todo debug
-    --logMsg("b0 = "..b[0])
-    --logMsg("b1 = "..b[1])
-    --logMsg("b2 = "..b[2])
-    --logMsg("b3 = "..b[3])
+    local b = data_from_string_swapped_efis(4, baro_r)
 
     local bl = {}
     for _, flag in pairs(lcd_flags_efisr) do
@@ -595,6 +578,9 @@ function draw_efisr_lcd(fcu, baro_r)
         end
         bl[flag.byte] = bit.bor(bl[flag.byte] ,(flag.mask *flag.value))
     end
+
+    print("lcd_flags_efisr = "..lcd_flags_efisr["unitInHg"].value)
+    print("bl2 = "..bl[2])
 
     local pkg_nr = 1
     hid_write(fcu, 0, 0xf0, 0x0, pkg_nr, 0x1a, 0x0e, 0xbf, 0x0, 0x0, 0x2, 0x1, 0x0, 0x0, 0xff, 0xff, 0x1d, 0x0, 0x0, 0x09, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
@@ -614,7 +600,7 @@ function draw_fcu_lcd(fcu ,spd, hdg, alt, vs)
     local a = data_from_string(5, alt, true)
     local v = data_from_string(4, vs, true)
 
-    logMsg("h = "..h[0].."-"..h[1].."-"..h[2])
+    --logMsg("h = "..h[0].."-"..h[1].."-"..h[2])
 
     local bl = {}
     for _, flag in pairs(lcd_flags_fcu) do
@@ -688,9 +674,7 @@ function refresh_dataref()
     else 
         lcd_flags_fcu["vs_vert"].value = 1
     end
-    --baro_r
-    logMsg("baro_r = "..cache_data["baro_rrr"])
-    local baro_r = cache_data["baro_rrr"] 
+    
 
     lcd_flags_fcu["fpa_comma"].value = 0
     --signal flags 
@@ -711,8 +695,6 @@ function refresh_dataref()
     local str_hdg = fix_str_len(hdg,3)
     local str_alt = fix_str_len(alt,5)
     local str_vs = fix_str_len(vs,4)
-    --efis_r
-    local str_baro_r = fix_str_len(baro_r*100,4) 
 
     --manage
     lcd_flags_fcu['spd_managed'].value = 0
@@ -747,6 +729,34 @@ function refresh_dataref()
         set_button_assignment(FCU_BUTTON_BEGIN+btn["VS_DEC"].id, "laminar/A333/autopilot/fpa_decrease")
         set_button_assignment(FCU_BUTTON_BEGIN+btn["VS_INC"].id, "laminar/A333/autopilot/fpa_increase")
     end
+
+    --EFIS R--
+    --QFE marker
+    --always off, it has never been observed in the sim
+    lcd_flags_efisr["qfe"].value = 0
+    --Baro
+    local baro_value_r = cache_data["baro_value_r"] 
+    local str_baro_r = ""
+    local unitIsInHg = (cache_data["baro_inhg_r"] == 0)
+    local isQnh = (cache_data["baro_mode_r"] == 0)
+    if(not isQnh) then
+        --Case: Std
+        lcd_flags_efisr["qnh"].value = 0
+        lcd_flags_efisr["unitInHg"].value = 0
+        str_baro_r = "STD " 
+    elseif(isQnh and unitIsInHg) then
+        --Case: Local baro in inHg
+        lcd_flags_efisr["unitInHg"].value = 1
+        lcd_flags_efisr["qnh"].value = 1
+        str_baro_r = fix_str_len(baro_value_r*100,4) --scales by factor 100 to get rid of the point in e.g. 29.92
+    elseif(isQnh and not unitIsInHg) then
+        --Case: Local baro in hPa
+        lcd_flags_efisr["unitInHg"].value = 0
+        lcd_flags_efisr["qnh"].value = 1
+        str_baro_r = round(baro_value_r * 33.86389) --convert from inhg to hpa
+        str_baro_r = rjust(tostring(str_baro_r),4,'0')
+    end
+
     --hid_open
     local fcu = hid_open(0x4098, 0xba01)
     draw_fcu_lcd(fcu, str_spd, str_hdg, str_alt, str_vs)
